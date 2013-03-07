c4b70c2dd08b9d26e1a810f6137f067b56e35f37
deb <deb@deb-BN.ka.hist.no>
2013-03-07 13:41:48 +0100

    Han helvett

--- Gruppe11-2013/src/java/com/corejsf/bestilling/plukkListeBean.java
+++ Gruppe11-2013/src/java/com/corejsf/bestilling/plukkListeBean.java
@@ -6,17 +6,15 @@ package com.corejsf.bestilling;
 import com.corejsf.DBadm.DBController;
 import com.corejsf.rett;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
-import java.util.ArrayList;  
-import java.util.List;  
-import javax.faces.application.FacesMessage;  
-import javax.faces.context.FacesContext;  
 import javax.swing.ImageIcon;
- 
-  
+import org.primefaces.event.TransferEvent;
 import org.primefaces.model.DualListModel;
-import org.primefaces.event.
 
 /**
  *
@@ -28,9 +26,9 @@ public class plukkListeBean extends DBController implements Serializable{
     
     
     
-    private DualListModel<rett> players;  
+    private DualListModel<rett> retter;  
       
-    private DualListModel<String> cities;  
+    private DualListModel<String> meny;  
   
     public plukkListeBean() {  
         //Retter  
@@ -44,9 +42,9 @@ public class plukkListeBean extends DBController implements Serializable{
         source.add(new rett(5, "mmmm", "godMat",new ImageIcon(getClass().getResource("/rettBilder/reker.jpg"), "namnam")));  
         source.add(new rett(6, "hellvett", "godMat",new ImageIcon(getClass().getResource("/rettBilder/godt.jpg"), "namnam")));      
           
-        players = new DualListModel<rett>(source, target);  
+        retter = new DualListModel<rett>(source, target);  
           
-        //Cities  
+        //Menyer 
         List<String> citiesSource = new ArrayList<String>();  
         List<String> citiesTarget = new ArrayList<String>();  
           
@@ -56,24 +54,24 @@ public class plukkListeBean extends DBController implements Serializable{
         citiesSource.add("Antalya");  
         citiesSource.add("Bursa");  
           
-        cities = new DualListModel<String>(citiesSource, citiesTarget);  
+        meny = new DualListModel<String>(citiesSource, citiesTarget);  
     }  
       
-    public DualListModel<rett> getPlayers() {  
-        return players;  
+    public DualListModel<rett> getRetter() {  
+        return retter;  
     }  
-    public void setPlayers(DualListModel<rett> players) {  
-        this.players = players;  
+    public void setRetter(DualListModel<rett> retter) {  
+        this.retter = retter;  
     }  
       
-    public DualListModel<String> getCities() {  
-        return cities;    
+    public DualListModel<String> getMenyer() {  
+        return meny;    
 }  
-    public void setCities(DualListModel<String> cities) {  
-        this.cities = cities;  
+    public void setMeny(DualListModel<String> meny) {  
+        this.meny = meny;  
     }  
       
-    public void onTransfer(TransferEvent event) {  
+    public void paaTransfer(TransferEvent event) {  
         StringBuilder builder = new StringBuilder();  
         for(Object item : event.getItems()) {  
             builder.append(((rett) item).getNavn()).append("<br />");  
@@ -81,7 +79,7 @@ public class plukkListeBean extends DBController implements Serializable{
           
         FacesMessage msg = new FacesMessage();  
         msg.setSeverity(FacesMessage.SEVERITY_INFO);  
-        msg.setSummary("Items Transferred");  
+        msg.setSummary("Rettvalg Bekreftet");  
         msg.setDetail(builder.toString());  
           
         FacesContext.getCurrentInstance().addMessage(null, msg);  
